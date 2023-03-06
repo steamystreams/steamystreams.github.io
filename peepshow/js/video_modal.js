4260c728b133b9ab2450101bb758eec39c156346
Craig <84981758+steamystreams@users.noreply.github.com>
2023-03-06 08:12:56 -0800

    Add files via upload

---
+++ peepshow/js/video_modal.js
@@ -0,0 +1,48 @@
+jQuery.fn.center = function() {
+    this.css("position", "absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+    return this;
+}
+
+
+$(document).ready(function() {
+
+    $(".video").css({
+        "width": $("#player").css("width"),
+        "height": $("#player").css("height")
+    });
+
+    $(".button").click(function() {
+        $(".video-wrapper").fadeIn('fast', function() {
+            $(".video").fadeIn();
+            $(".video").center();
+        });
+
+    });
+
+    $(".video-wrapper").click(function(e) {
+        if ($(e.target).is(".video-wrapper")) {
+            $(".video").fadeOut(function() {
+                $(".video-wrapper").fadeOut(function() {
+                    $(".video, .video-wrapper").css({
+                        'display': 'none'
+                    });
+                    var src = $("#player").attr("src");
+                    $("#player").attr("src", "");
+                    $("#player").attr("src", src);
+                });
+            });
+        }
+    });
+
+    $(document).keyup(function(e) {
+        var isShown = $(".video-wrapper").css("display");
+
+        if (isShown !== "none" && e.which == 27) {
+            $(".video-wrapper").click();
+        }
+
+    });
+
+});
