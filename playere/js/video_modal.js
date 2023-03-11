e1d03ca2f29db66e696af8b949e268dcbcf45b2a
Craig <84981758+steamystreams@users.noreply.github.com>
2023-03-11 02:19:02 -0800

    Add files via upload

--- playere/js/video_modal.js
+++ playere/js/video_modal.js
@@ -1,46 +1,46 @@
 jQuery.fn.center = function () {
-    this.css("position","absolute");
-    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + $(window).scrollTop()) + "px");
-    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + $(window).scrollLeft()) + "px");
-    return this;
+  this.css("position","absolute");
+  this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + $(window).scrollTop()) + "px");
+  this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+  return this;
 }
-  
-  
+
+
 $(document).ready(function() {
-  
-  $(".video").css({
-    "width"  : $("#player").css("width"),
-    "height" : $("#player").css("height")
+
+$(".video").css({
+  "width"  : $("#player").css("width"),
+  "height" : $("#player").css("height")
+});
+
+$(".button").click(function() {
+  $(".video-wrapper").fadeIn('fast', function() {
+    $(".video").fadeIn(); 
+    $(".video").center(); 
   });
-  
-  $(".button").click(function() {
-    $(".video-wrapper").fadeIn('fast', function() {
-      $(".video").fadeIn(); 
-      $(".video").center(); 
-    });
-        
-  });
-  
-  $(".video-wrapper").click(function(e) {
-    if($(e.target).is(".video-wrapper")) {
-      $(".video").fadeOut(function() {
-        $(".video-wrapper").fadeOut(function() {
-         $(".video, .video-wrapper").css({'display':'none'}); 
-          let src=$("#player").attr("src");
-          $("#player").attr("src", "");
-          $("#player").attr("src", src);
-        });
+      
+});
+
+$(".video-wrapper").click(function(e) {
+  if($(e.target).is(".video-wrapper")) {
+    $(".video").fadeOut(function() {
+      $(".video-wrapper").fadeOut(function() {
+       $(".video, .video-wrapper").css({'display':'none'}); 
+        var src=$("#player").attr("src");
+        $("#player").attr("src", "");
+        $("#player").attr("src", src);
       });
-    }
-  });
+    });
+  }
+});
+
+$(document).keyup(function(e) {
+  var isShown=$(".video-wrapper").css("display");
   
-  $(document).keyup(function(e) {
-    let isShown=$(".video-wrapper").css("display");
-    
-    if(isShown !== "none" && e.which==27) {
-       $(".video-wrapper").click(); 
-    }
-    
-  });
+  if(isShown !== "none" && e.which==27) {
+     $(".video-wrapper").click(); 
+  }
   
+});
+
 });
