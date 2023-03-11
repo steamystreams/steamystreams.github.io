fc071941445eb14dd42de80ca0c6e9b5c789b73c
Craig <84981758+steamystreams@users.noreply.github.com>
2023-03-10 23:53:12 -0800

    Update video_modal.js

--- playere/js/video_modal.js
+++ playere/js/video_modal.js
@@ -1,3 +1,4 @@
+var jQuery = require('jQuery.js');
 jQuery.fn.center = function () {
     this.css("position","absolute");
     this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + $(window).scrollTop()) + "px");
